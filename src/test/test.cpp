7f7d0ea7bb2e123b756ad66cfb9c79656a6bc93e
placeholder <invalid@example.com>
2023-10-27 19:29:15 +0200

    DO NOT SHOW TO PROFS

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,5 +1,6 @@
 #include "SFML/Graphics/Color.hpp"
 #include "SFML/Window/Event.hpp"
+#include "circle.hpp"
 #include "engine.hpp"
 #include "point2d.hpp"
 #include <algorithm>
@@ -9,6 +10,33 @@
 
 void customLoopFunction() {}
 
+void addSomeRenderables() {
+  Engine &eng = Engine::getInstance();
+
+  LineSegment *l =
+      new LineSegment{{70 + 85, 10}, {70 + 85, 120}, sf::Color::Yellow};
+  eng.add(l);
+
+  Circle *circle{new Circle{}};
+  circle->setRadius(45).setPosition({70, 100}).setFillColor(sf::Color::Yellow);
+  eng.add(circle);
+
+  Circle *circle2{new Circle{}};
+  circle2->setRadius(45)
+      .setPosition({150, 100})
+      .setFillColor(sf::Color::Yellow);
+  eng.add(circle2);
+}
+
+void setUpCustomEvents() {
+  Engine::getInstance().setEventHandler(
+      sf::Event::MouseButtonPressed, [](const sf::Event &ev) {
+        std::cout << "Custom event handler Mouse button press "
+                  << ev.mouseButton.button << '\t' << ev.mouseButton.x << '\t'
+                  << ev.mouseButton.y << "\n";
+      });
+}
+
 int main() {
   Engine &eng = Engine::getInstance()
                     .setWindowTitle("dev")
@@ -16,14 +44,8 @@ int main() {
                     .setLoopFunction(customLoopFunction)
                     .buildWindow();
 
-  eng.setEventHandler(sf::Event::MouseButtonPressed, [](const sf::Event &ev) {
-    std::cout << "Custom event handler Mouse button press "
-              << ev.mouseButton.button << '\t' << ev.mouseButton.x << '\t'
-              << ev.mouseButton.y << "\n";
-  });
-
-  LineSegment *l = new LineSegment{{10, 10}, {100, 200}, sf::Color::Yellow};
-  eng.add(l);
+  setUpCustomEvents();
+  addSomeRenderables();
 
   eng.loop();
 }
