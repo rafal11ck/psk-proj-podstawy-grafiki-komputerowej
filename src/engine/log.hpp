9c42ef47be3ba2f22b551d477ea707c3a4c048e2
placeholder <invalid@example.com>
2023-11-05 14:01:43 +0100

    trace macro improvments

--- src/engine/log.hpp
+++ src/engine/log.hpp
@@ -31,8 +31,9 @@ extern std::ostream &logstream;
 
 #ifdef TRACE
 #define LOGTRACEN                                                              \
-  G::logstream << "\033[35m" << __FILE_NAME__ << " \033[36m"                   \
-               << "\033[0m " << __PRETTY_FUNCTION__ << " TRACE\n";
+  G::logstream << "TRACE "                                                     \
+               << "\033[35m" << __FILE_NAME__ << " \033[36m"                   \
+               << __PRETTY_FUNCTION__ << "\033[0m\n";
 #else
 #define LOGTRACEN
 #endif // TRACE
