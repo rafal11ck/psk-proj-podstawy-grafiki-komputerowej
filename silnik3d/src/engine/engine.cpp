66b30149d2e643c4ecdb10726ee79beb29ee71ff
placeholder <invalid@example.com>
2023-12-22 14:48:55 +0100

    refactor

--- silnik3d/src/engine/engine.cpp
+++ silnik3d/src/engine/engine.cpp
@@ -1,27 +1,11 @@
 #include "SFML/Window/ContextSettings.hpp"
+#include "SFML/Window/Event.hpp"
 #include <GL/glew.h>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
 
-const char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 int main() {
-
-  std::cout << "Hi\n";
   sf::ContextSettings settings{24, 8, 4, 3, 3, settings.Core};
 
   // create the window
@@ -32,92 +16,13 @@ int main() {
   // activate the window
   window.setActive(true);
 
+  // Opengl extension load
   glewInit();
 
   // load resources, initialize the OpenGL states, ...
   // build and compile our shader program
   // ------------------------------------
   // vertex shader
-
-  std::cout << "Hi1.1\n";
-  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-  std::cout << "Hi2\n";
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
-  // check for shader compile errors
-  int success;
-  char infoLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-              << infoLog << std::endl;
-  }
-  // fragment shader
-  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-  // check for shader compile errors
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-  if (!success) {
-    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-              << infoLog << std::endl;
-  }
-  // link shaders
-  unsigned int shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-  // check for linking errors
-  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-  if (!success) {
-    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
-              << infoLog << std::endl;
-  }
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
-
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-  // ------------------------------------------------------------------
-  float verticesA[] = {
-      -0.8f, 0.0f,
-      0.0f, // ll
-      -0.4f, 0.8f,
-      0.0f, // lt
-      0.0f,  -0.0f,
-      0.0f // center
-  };
-
-  float verticesB[] = {
-      0.0f, -0.0f,
-      0.0f, // center
-      0.8f, 0.0f,
-      0.0f, // rr
-      0.4,  0.8,
-      0.0f // rt
-  };
-
-  unsigned int VAOs[2];
-  glGenVertexArrays(2, VAOs);
-  unsigned int VBOs[2];
-  glGenBuffers(2, VBOs);
-
-  glBindVertexArray(VAOs[0]);
-  glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(verticesA), verticesA, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-  glEnableVertexAttribArray(0);
-  glBindVertexArray(0);
-
-  glBindVertexArray(VAOs[1]);
-  glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(verticesB), verticesB, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-  glEnableVertexAttribArray(0);
-  glBindVertexArray(0);
 
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   glLineWidth(5);
@@ -142,18 +47,6 @@ int main() {
     // clear the buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // draw...
-
-    glUseProgram(shaderProgram);
-    // seeing as we only have a single VAO there's no need to bind it
-    // every time, but we'll do so to keep things a bit more organized
-    glBindVertexArray(VAOs[0]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glBindVertexArray(VAOs[1]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    // glBindVertexArray(0);
-
     // end the current frame (internally swaps the front and back buffers)
     window.display();
   }
@@ -161,9 +54,6 @@ int main() {
   // release resources...
   // optional: de-allocate all resources once they've outlived their purpose:
   // ------------------------------------------------------------------------
-  glDeleteVertexArrays(2, VAOs);
-  glDeleteBuffers(2, VBOs);
-  glDeleteProgram(shaderProgram);
 
   return 0;
 }
---
+++ silnik3d/src/engine/shader.hpp
@@ -0,0 +1,42 @@
+#ifndef SHADER_HPP_
+#define SHADER_HPP_
+
+#include <fstream>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+class Shader {
+private:
+  using ShaderType = enum SHADER_TYPE { VERTEX, FRAGMENT };
+  using Exception = enum EXCEPTION { COMPILE, LINK };
+  GLuint fragmentShader;
+  GLuint vertexShader;
+  GLuint id;
+
+  std::string loadShaderFromFile(const char *path);
+  GLuint createShader(const char *path, ShaderType type);
+  GLuint createProgram();
+  void checkForException(GLuint program, Exception exceptionType);
+
+public:
+  Shader(const char *vertexPath, const char *fragmentPath);
+
+  void use();
+  void setBool(const char *name, bool value);
+  void setInt(const char *name, int value);
+  void setFloat(const char *name, float value);
+  void setVec2(const char *name, const glm::vec2 &value);
+  void setVec2(const char *name, float x, float y);
+  void setVec3(const char *name, const glm::vec3 &value);
+  void setVec3(const char *name, float x, float y, float z);
+  void setVec4(const char *name, const glm::vec4 &value);
+  void setVec4(const char *name, float x, float y, float z, float w);
+  void setMat2(const char *name, const glm::mat2 &mat);
+  void setMat3(const char *name, const glm::mat3 &mat);
+  void setMat4(const char *name, const glm::mat4 &mat);
+};
+
+#endif // SHADER_HPP_
