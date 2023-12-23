578393e48c676c1ade5e120390c2cabd8299cc50
placeholder <invalid@example.com>
2023-12-23 15:55:35 +0100

    reorganize

---
+++ silnik3d/engine/engine.cpp
@@ -0,0 +1,146 @@
+// @clang-format off
+#include <GL/glew.h>
+// @clang-format on
+#include "SFML/Window/ContextSettings.hpp"
+#include "SFML/Window/Event.hpp"
+#include "shader.hpp"
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <filesystem>
+#include <iostream>
+#include <stb_image.h>
+
+int main() {
+
+  sf::ContextSettings settings{24, 8, 4, 3, 3, settings.Core};
+
+  // create the window
+  sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default,
+                    settings);
+  window.setVerticalSyncEnabled(true);
+
+  // activate the window
+  window.setActive(true);
+
+  // Opengl extension load
+  glewInit();
+
+  // build and compile our shader program
+  // ------------------------------------
+  Shader ourShader(
+      "vertex.glsl",
+      "fragment.glsl"); // you can name your shader files however you like
+
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  float vertices[] = {
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // load and create a texture
+  // -------------------------
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D,
+                texture); // all upcoming GL_TEXTURE_2D operations now have
+                          // effect on this texture object
+  // set the texture wrapping parameters
+  glTexParameteri(
+      GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+      GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannels;
+  // The FileSystem::getPath(...) is part of the GitHub repository so we can
+  // find files on any IDE/platform; replace it with your own image path.
+  unsigned char *data =
+      stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(),
+                &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // glLineWidth(5);
+
+  // run the main loop
+  bool running = true;
+  while (running) {
+    // handle events
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        // end the program
+        running = false;
+      } else if (event.type == sf::Event::Resized) {
+        // adjust the viewport when the window is resized
+        glViewport(0, 0, event.size.width, event.size.height);
+      }
+    }
+
+    // color
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // clear the buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    ourShader.use();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // end the current frame (internally swaps the front and back buffers)
+    window.display();
+  }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+
+  // release resources...
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+
+  return 0;
+}
