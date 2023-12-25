4952f2eb5738695da19fc2a0945fca10995b193a
placeholder <invalid@example.com>
2023-12-25 17:38:49 +0100

    rotate cubes

--- silnik3d/engine/engine.cpp
+++ silnik3d/engine/engine.cpp
@@ -61,6 +61,13 @@ int main() {
       0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
       -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
 
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+
   unsigned int indices[] = {
       0, 1, 3, // first triangle
       1, 2, 3  // second triangle
@@ -181,11 +188,6 @@ int main() {
     ourShader.use();
 
     // create transformations
-    glm::mat4 model = glm::mat4(
-        1.0f); // make sure to initialize matrix to identity matrix first
-    model = glm::rotate(
-        model, (float)clock.getElapsedTime().asSeconds() * glm::radians(50.0f),
-        glm::vec3(0.5f, 1.0f, 0.0f));
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     glm::mat4 projection = glm::mat4(1.0f);
@@ -196,6 +198,7 @@ int main() {
     unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
     unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
     // pass them to the shaders (3 different ways)
+    glm::mat4 model = glm::mat4(1.0f);
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
     // note: currently we set the projection matrix each frame, but since the
@@ -205,8 +208,21 @@ int main() {
 
     // render container
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    for (unsigned int i = 0; i < 10; i++) {
+      model = glm::mat4(1.0f);
+      // glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f;
+      model = glm::rotate(model,
+                          clock.getElapsedTime().asSeconds() *
+                                  glm::radians(angle) * (i + 1) / 3 +
+                              0.5f + i * 2,
+                          glm::vec3(1.0f, 0.3f, 0.5f));
+      ourShader.setMat4("model", model);
+
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
     //  end the current frame (internally swaps the front and back buffers)
     window.display();
   }
