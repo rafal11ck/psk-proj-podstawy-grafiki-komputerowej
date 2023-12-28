030003b176f1c835834a500f091a455dfcd77826
placeholder <invalid@example.com>
2023-12-28 16:36:48 +0100

    make enigne global

--- silnik3d/src/test/main.cpp
+++ silnik3d/src/test/main.cpp
@@ -1,4 +1,15 @@
 #include "engine.hpp"
+#include <SFML/OpenGL.hpp>
 #include <iostream>
 
-int main() { Engine &engine{Engine::getInstance()}; }
+Engine &engine{Engine::getInstance()};
+
+int main() {
+
+  engine.setLoopFunction([]() {
+    glClearColor(0.0, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  });
+
+  engine.loop();
+}
