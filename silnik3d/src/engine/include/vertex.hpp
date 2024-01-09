7585350cabc893eb365ceb2cbc703c75e9b617d0
rafal11ck <76795799+rafal11ck@users.noreply.github.com>
2024-01-09 09:06:39 +0100

    mesh header

--- silnik3d/src/engine/include/vertex.hpp
+++ silnik3d/src/engine/include/vertex.hpp
@@ -1,10 +1,12 @@
 #ifndef VERTEX_HPP_
 #define VERTEX_HPP_
 
+#include <glm/glm.hpp>
+
 struct Vertex {
-  glm::vec3 Position;
-  glm::vec3 Normal;
-  glm::vec2 TexCoords;
+  glm::vec3 m_position;
+  glm::vec3 m_normal;
+  glm::vec2 m_texCoords;
 };
 
 #endif // VERTEX_HPP_
